9ee0867dc8e30e085412c21137024258731add13
Andi Chaerul <andichaerul85@gmail.com>
2020-09-06 21:26:27 +0800

    Api available unit, enable xdebug create json file

--- app/Http/Controllers/FindArmada.php
+++ app/Http/Controllers/FindArmada.php
@@ -2,29 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
+use DatePeriod;
+use DateInterval;
+use App\ModelOrders;
 use App\ModelDaftarArmada;
 
 class FindArmada extends Controller
 {
-    public function Index()
+    public function Index($tglStart, $tglFinish)
     {
-        $data = ModelDaftarArmada::all();
-        foreach ($data as $row) {
-            foreach ($row->gambarArmada as $urlGambar) {
-                $urlGambarArr[$row->id][] = $urlGambar->url;
+        $idArmadaReady = $this->_findArmadaReady($tglStart, $tglFinish);
+        $return = [];
+        for ($i = 0; $i < count($idArmadaReady); $i++) {
+            $detailArmada = ModelDaftarArmada::find($idArmadaReady[$i]);
+            foreach ($detailArmada->gambarArmada as $urlGambar) {
+                $urlGambarArr[$detailArmada->id][] = $urlGambar->url;
             }
             $return[] = array(
-                'namaMitra' => (empty($row->mitra)) ? null : $row->mitra->nama_mitra,
-                'unitBrand' => (empty($row->varian)) ? null : $row->varian->brand->namaBrand,
-                'namaUnit' => (empty($row->varian)) ? null : $row->varian->namaVarian,
-                'typeVarian' => (empty($row->varian)) ? null : $row->varian->typeUnit,
-                'warna' => (empty($row->varian)) ? null : $row->varian->warna,
-                'isMatic' => (empty($row->varian)) ? null : $row->isMatic,
-                'tahunKendaraan' => (empty($row)) ? null : $row->tahunArmada,
-                'isIncludeDriver' => (empty($row)) ? null : $row->isIncludeDriver,
-                'urlGambar' => $urlGambarArr[$row->id]
+                'namaMitra' => (empty($detailArmada->mitra)) ? null : $detailArmada->mitra->nama_mitra,
+                'unitBrand' => (empty($detailArmada->varian)) ? null : $detailArmada->varian->brand->namaBrand,
+                'namaUnit' => (empty($detailArmada->varian)) ? null : $detailArmada->varian->namaVarian,
+                'typeVarian' => (empty($detailArmada->varian)) ? null : $detailArmada->varian->typeUnit,
+                'warna' => (empty($detailArmada->varian)) ? null : $detailArmada->varian->warna,
+                'isMatic' => (empty($detailArmada->varian)) ? null : $detailArmada->varian->isMatic,
+                'tahunKendaraan' => (empty($row)) ? null : $detailArmada->tahunArmada,
+                'isIncludeDriver' => (empty($row)) ? null : $detailArmada->isIncludeDriver,
+                'urlGambar' => $urlGambarArr[$detailArmada->id]
             );
         }
         return $return;
+    }
+
+    public function _tglPemakaian($tglStart, $tglFinish)
+    {
+        $begin = new DateTime($tglStart);
+        $end = new DateTime($tglFinish);
+        $end = $end->modify('+1 day');
+        $interval = new DateInterval('P1D');
+        $daterange = new DatePeriod($begin, $interval, $end);
+        foreach ($daterange as $date) {
+            $test[] = $date->format("Y-m-d");
+        }
+        if (empty($test)) {
+            $error = array(
+                'status' => 'error',
+            );
+            die(json_encode($error));
+        } else {
+            return $test;
+        }
+    }
+
+    public function _findArmadaReady($tglStart, $tglFinish)
+    {
+        $idArmadaReady = [];
+        $data = ModelOrders::all();
+        $tglPemakaian = $this->_tglPemakaian($tglStart, $tglFinish);
+        foreach ($data as $row) {
+            $id = $row->id;
+            $begin = new DateTime($row->dariTanggal);
+            $end = new DateTime($row->sampaiTanggal);
+            $end = $end->modify('+1 day');
+
+            $interval = new DateInterval('P1D');
+            $daterange = new DatePeriod($begin, $interval, $end);
+            foreach ($daterange as $date) {
+                $test[$id][] = $date->format("Y-m-d");
+            }
+            for ($i = 0; $i < count($tglPemakaian); $i++) {
+                $dede[$i] = array_search($tglPemakaian[$i], $test[$id]);
+                if (array_search($tglPemakaian[$i], $test[$id]) === false) {
+                    null;
+                } else {
+                    $tidakTersedia[$id] = $id;
+                }
+            }
+            if (empty($tidakTersedia[$id])) {
+                $idArmadaReady[] = $id;
+            }
+        }
+        return $idArmadaReady;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 http://localhost/ainun-rent/api/
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('v1/offers','ApiOffers@Index');
-Route::get('v1/find_armada','FindArmada@Index');
+Route::get('v1/offers', 'ApiOffers@Index');
+Route::get('v1/find_armada/{tglStart}/{tglFinish}', 'FindArmada@Index');
